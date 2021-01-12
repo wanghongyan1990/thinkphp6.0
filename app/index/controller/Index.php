09a4b983dcb727d8f804fba2dd89e2561976c2b3
wanghongyan1990 <1085946467@qq.com>
2021-01-12 22:54:26 +0800

    first

---
+++ app/index/controller/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace app\index\controller;
+use app\BaseController;
+use think\facade\Db;
+//use think\Db;
+
+class Index extends BaseController
+{
+  public function index(){
+      //输出数据库中的信息
+      $select = Db::table('hy_uadmin') -> where('uid',1) -> find();
+      return json_encode($select);
+//    return view();
+  }
+
+  public function adb(){
+      return '输出了adb';
+  }
+
+    public function hello(string $name)
+    {
+        return 'Hello,'.$name;
+    }
+}
---
+++ app/index/controller/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\index\controller;
+use app\BaseController;
+
+class Test extends BaseController
+{
+    public  function index(){
+        echo 'haode';
+    }
+
+    public function Test(){
+        echo 'nihao';
+    }
+}
